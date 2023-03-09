b68c31c602d276c9636443455be2d41620eea42d
Andres Macedo <andres2020iius@inspiredintellect-us.com>
2023-03-09 07:23:00 -0700

    Added prettier and autologin

--- public/vanilla-js/scripts/Auth.js
+++ public/vanilla-js/scripts/Auth.js
@@ -2,79 +2,95 @@ import API from "./API.js";
 import Router from "./Router.js";
 
 const Auth = {
-    isLoggedIn: false,
-    account: null,
-    postLogin: (response, user)=>{
-        console.log('post login')
-        if(response.ok){
-            Auth.isLoggedIn = true
-            Auth.account = user
-            Auth.updateStatus()
-            Router.go("/account")
+  isLoggedIn: false,
+  account: null,
+  postLogin: (response, user) => {
+    if (response.ok) {
+      Auth.isLoggedIn = true;
+      Auth.account = user;
+      Auth.updateStatus();
+      Router.go("/account");
+    } else {
+      alert(response.message);
+    }
+    if (window.PasswordCredential && user.password) {
+      console.log("storing");
+      const credentials = new PasswordCredential({
+        id: user.email,
+        password: user.password,
+        name: user.name,
+      });
+      navigator.credentials.store(credentials);
+    }
+  },
+  register: async (event) => {
+    event.preventDefault();
+    const user = {
+      name: document.getElementById("register_name").value,
+      email: document.getElementById("register_email").value,
+      password: document.getElementById("register_password").value,
+    };
+    const response = await API.register(user);
+    Auth.postLogin(response, user);
+  },
+  login: async (event) => {
+    if (event) event.preventDefault();
+    const credentials = {
+      email: document.getElementById("login_email").value,
+      password: document.getElementById("login_password").value,
+    };
+    console.log("here in login");
+    const response = await API.login(credentials);
+    Auth.postLogin(response, { ...credentials, name: response.name });
+  },
+  autoLogin: async () => {
+    if (window.PasswordCredential) {
+      const credentials = await navigator.credentials.get({ password: true });
+      console.log(credentials);
+      document.getElementById("login_email").value = credentials.id;
+      document.getElementById("login_password").value = credentials.password;
+      Auth.login();
+    }
+  },
+  logout: () => {
+    Auth.isLoggedIn = false;
+    Auth.account = null;
+    Auth.updateStatus();
+    Router.go("/");
 
-        } else {
-            alert(response.message)
-        }
-    },
-    register: async (event)=> {
-        event.preventDefault()
-        const user = {
-            name : document.getElementById("register_name").value,
-            email : document.getElementById("register_email").value,
-            password : document.getElementById("register_password").value,
-        }
-        const response = await API.register(user)
-        Auth.postLogin(response, {name: user.name, email: user.email})
-        console.log('response', response)
-    },
-    login: async (event)=> {
-        event.preventDefault()
-        const credentials = {
-           
-            email : document.getElementById("login_email").value,
-            password : document.getElementById("login_password").value,
-        }
-        const response = await API.login(credentials)
-        console.log('response', response)
-        Auth.postLogin(response, {...credentials, name: response.name})
-        
-    },
-    logout: ()=>{
-        Auth.isLoggedIn = false
-        Auth.account = null
-        Auth.updateStatus()
-        Router.go("/")
-    },
-    updateStatus() {
-        if (Auth.isLoggedIn && Auth.account) {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "none"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".account_name").forEach(
-                e => e.innerHTML = Auth.account.name
-            );
-            document.querySelectorAll(".account_username").forEach(
-                e => e.innerHTML = Auth.account.email
-            );
+    if (window.PasswordCredential) {
+      // if user logs out we prevent autologin next time
+      navigator.credentials.preventSilentAccess();
+    }
+  },
+  updateStatus() {
+    if (Auth.isLoggedIn && Auth.account) {
+      document
+        .querySelectorAll(".logged_out")
+        .forEach((e) => (e.style.display = "none"));
+      document
+        .querySelectorAll(".logged_in")
+        .forEach((e) => (e.style.display = "block"));
+      document
+        .querySelectorAll(".account_name")
+        .forEach((e) => (e.innerHTML = Auth.account.name));
+      document
+        .querySelectorAll(".account_username")
+        .forEach((e) => (e.innerHTML = Auth.account.email));
+    } else {
+      document
+        .querySelectorAll(".logged_out")
+        .forEach((e) => (e.style.display = "block"));
+      document
+        .querySelectorAll(".logged_in")
+        .forEach((e) => (e.style.display = "none"));
+    }
+  },
+  init: () => {},
+};
 
-        } else {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "none"
-            );
-
-        }
-    },    
-    init: () => {
-        
-    },
-}
 Auth.updateStatus();
+Auth.autoLogin();
 
 export default Auth;
 
